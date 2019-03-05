0f5ad080db41a32ff251e299ddb6f6e5fd7e63b8
Catarina Machado <catarinamachado11@gmail.com>
2019-03-05 16:11:48 +0000

    Add remove utentes, serviços e consultas (#2)

--- exercicio1/state.pl
+++ exercicio1/state.pl
@@ -1,5 +1,14 @@
-utente(1,carlos,31,porto).
-utente(2,joao,20,braga).
-utente(3,vilaca,20,braga).
-servico(1,acs,hpb,braga).
-consulta(1,1,1,10).
+% utente: #IdUt,Nome,Idade,Cidade -> {V,F}
+utente(1,'Carlos',2,'Porto').
+utente(2,'Joao',20,'Braga').
+utente(3,'Vilaca',20,'Braga').
+utente(4,'Catarina',20,'Braga').
+
+% serviço: #IdServ,Descrição,Instituição,Cidade -> {V,F}
+servico(1,'Pediatria','Hospital Santa Maria','Porto').
+servico(2,'Ortopedia','Hospital de Braga','Braga').
+
+% consulta: Data,#IdUt,#IdServ,Custo -> {V,F}
+consulta('01-01-2010',1,1,10).
+consulta('01-02-2010',4,2,20).
+consulta('01-03-2010',2,2,40).
