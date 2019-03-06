71c4b97120fbc23f5395ba4b6a55c12aaaa11010
Catarina Machado <catarinamachado11@gmail.com>
2019-03-06 12:20:32 +0000

    Edit template of saved data

--- exercicio1/exercicio1.pl
+++ exercicio1/exercicio1.pl
@@ -53,26 +53,29 @@ nao(Q) :- Q,!,fail.
 nao(Q).
 
 saveUtente(Stream) :- utente(A,B,C,D),
-        write(Stream, 'utente('),write(Stream, A),write(Stream, ','),
-        write(Stream, B), write(Stream, ','), write(Stream, C),
-        write(Stream, ','), write(Stream, D), write(Stream, ').\n'),
+        write(Stream, 'utente('),write(Stream, A),write(Stream, ',\''),
+        write(Stream, B), write(Stream, '\','), write(Stream, C),
+        write(Stream, ',\''), write(Stream, D), write(Stream, '\').\n'),
     fail; true.
 
 saveServico(Stream) :- servico(A,B,C,D),
-        write(Stream, 'servico('),write(Stream, A),write(Stream, ','),
-        write(Stream, B), write(Stream, ','), write(Stream, C),
-        write(Stream, ','), write(Stream, D), write(Stream, ').\n'),
+        write(Stream, 'servico('),write(Stream, A),write(Stream, ',\''),
+        write(Stream, B), write(Stream, '\',\''), write(Stream, C),
+        write(Stream, '\',\''), write(Stream, D), write(Stream, '\').\n'),
     fail; true.
 
 saveConsulta(Stream) :- consulta(A,B,C,D),
-        write(Stream, 'consulta('),write(Stream, A),write(Stream, ','),
+        write(Stream, 'consulta(\''),write(Stream, A),write(Stream, '\','),
         write(Stream, B), write(Stream, ','), write(Stream, C),
         write(Stream, ','), write(Stream, D), write(Stream, ').\n'),
     fail; true.
 
 saveState :-
     open('state.pl', write, Stream),
+    write(Stream, '% utente: #IdUt,Nome,Idade,Cidade -> {V,F}\n'),
     saveUtente(Stream),
+    write(Stream, '\n% serviço: #IdServ,Descrição,Instituição,Cidade -> {V,F}\n'),
     saveServico(Stream),
+    write(Stream, '\n% consulta: Data,#IdUt,#IdServ,Custo -> {V,F}\n'),
     saveConsulta(Stream),
     close(Stream).
