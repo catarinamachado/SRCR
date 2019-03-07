aa80594cd22ab0eb246e026b59ddbbd6c61198ee
machadovilaca <machadovilaca@gmail.com>
2019-03-07 12:12:20 +0000

    Clean up

--- exercicio1/calculoDeCustos.pl
+++ exercicio1/calculoDeCustos.pl
@@ -1,18 +1,18 @@
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Calcular o custo total dos cuidados de saúde por utente/serviço/instituição/data
 
-totalUtente(IdUtente, Total) :- 
+totalUtente(IdUtente, Total) :-
     findall(Custo, consulta(_,IdUtente,_,Custo), Lista),
     soma(Lista, Total).
 
 
-totalServico(IdServ, Total) :- 
+totalServico(IdServ, Total) :-
     findall(Custo, consulta(_,_,IdServ,Custo), Lista),
     soma(Lista, Total).
 
 
 somaServicos([], 0).
-somaServicos([X|XS], Total) :- 
+somaServicos([X|XS], Total) :-
     somaServicos(XS, Acumulada),
     totalServico(X, TotalServico),
     Total is TotalServico + Acumulada.
@@ -20,8 +20,8 @@ somaServicos([X|XS], Total) :-
 totalInstituicao(NomeInstituicao, Total) :-
     findall(IdServico, servico(IdServico,_,NomeInstituicao,_), Lista),
     somaServicos(Lista, Total).
-    
-    
-totalData(Data, Total) :- 
+
+
+totalData(Data, Total) :-
     findall(Custo, consulta(Data,_,_,Custo), Lista),
     soma(Lista, Total).
