5d52fbf77e1f81f3c08eb8c712d47ca77a72b15f
Catarina Machado <catarinamachado11@gmail.com>
2019-03-10 18:51:20 +0000

    Identificar servicos e consultas  por criterios de selecao

--- exercicio1/identificacoes.pl
+++ exercicio1/identificacoes.pl
@@ -4,7 +4,6 @@
 identificaInstituicoes(R) :- findall(I, servico(_,_,I,_), Tmp),
                              eliminaRepetidos(Tmp, R).
 
-
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Identificar utentes/serviços/consultas por critérios de seleção;
 
@@ -17,3 +16,20 @@ utenteIdade(I, R) :- findall(utente(Id,N,I,C), utente(Id,N,I,C), R).
 
 utenteCidade(C, R) :- findall(utente(Id,N,I,C), utente(Id,N,I,C), R).
 
+% Serviços
+servicoID(Id,R) :- findall(servico(Id,D,I,C), servico(Id,D,I,C), R).
+
+servicoDescricao(D, R) :- findall(servico(Id,D,I,C), servico(Id,D,I,C), R).
+
+servicoInstituicao(I, R) :- findall(servico(Id,D,I,C), servico(Id,D,I,C), R).
+
+servicoCidade(C, R) :- findall(servico(Id,D,I,C), servico(Id,D,I,C), R).
+
+% Consultas
+consultaData(D,R) :- findall(consulta(D,IdU,IdS,C), consulta(D,IdU,IdS,C), R).
+
+consultaIdUtente(IdU, R) :- findall(consulta(D,IdU,IdS,C), consulta(D,IdU,IdS,C), R).
+
+consultaIdServico(IdS, R) :- findall(consulta(D,IdU,IdS,C), consulta(D,IdU,IdS,C), R).
+
+consultaCusto(C, R) :- findall(consulta(D,IdU,IdS,C), consulta(D,IdU,IdS,C), R).
