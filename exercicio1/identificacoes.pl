40551f0c2ac9510e2c6a4e7e1075e3891aec5c8e
Catarina Machado <catarinamachado11@gmail.com>
2019-03-10 18:36:18 +0000

    Identificar utentes por criterios de selecao

--- exercicio1/identificacoes.pl
+++ exercicio1/identificacoes.pl
@@ -3,3 +3,17 @@
 
 identificaInstituicoes(R) :- findall(I, servico(_,_,I,_), Tmp),
                              eliminaRepetidos(Tmp, R).
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Identificar utentes/serviços/consultas por critérios de seleção;
+
+% Utentes
+utenteID(Id, R) :- findall(utente(Id,N,I,C), utente(Id,N,I,C), R).
+
+utenteNome(N, R) :- findall(utente(Id,N,I,C), utente(Id,N,I,C), R).
+
+utenteIdade(I, R) :- findall(utente(Id,N,I,C), utente(Id,N,I,C), R).
+
+utenteCidade(C, R) :- findall(utente(Id,N,I,C), utente(Id,N,I,C), R).
+
