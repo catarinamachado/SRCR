891016f67d33b596aef02e72d0f5ae7291692c5e
Catarina Machado <catarinamachado11@gmail.com>
2019-03-13 15:48:01 +0000

    Add invariantes e predicado de evolução (#6)

--- exercicio1/funcoesAuxiliares.pl
+++ exercicio1/funcoesAuxiliares.pl
@@ -1,20 +1,36 @@
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Funções Auxiliares
 
+% Insucesso
+insucesso :- !, fail.
+
 % Calcula o valor de verdade contrário à resposta à questão
-nao(Q) :- Q,!,fail.
+nao(Q) :- Q, insucesso.
 nao(Q).
 
+% Encontra todas as soluções
+solucoes(X, XS, L) :- findall(X, XS, L).
 
-% Somatório
-soma([],0).
-soma([X|XS],Total) :- soma(XS, Acumulado), Total is X + Acumulado.
+% Comprimento de uma lista
+comprimento([], 0).
+comprimento([_|XS], R) :- comprimento(XS, S), R is 1 + S.
 
+% Inserção de conhecimento
+insercao(Q) :- assert(Q).
+insercao(Q) :- retract(Q), insucesso.
+
+% Testa se todos os meta-predicados são verdadeiros
+teste([]).
+teste([I|L]) :- I, teste(L).
+
+% Insere novo conhecimento na base de conhecimento
+evolucao(T) :- solucoes(I, +T::I, Linv),
+               insercao(T),
+               teste(Linv).
 
 % Averigua se elemento pertence a uma lista
 pertence(A,[A|XS]).
 pertence(A,[X|XS]) :- pertence(A,XS).
-
 
 % Elimina os elementos repetidos numa lista
 eliminaRepetidos(X, R) :- eliminaRepAux(X,[],R).
@@ -23,7 +39,10 @@ eliminaRepAux([],Acc,Acc).
 eliminaRepAux([X|XS],Acc,R) :- pertence(X,Acc), eliminaRepAux(XS,Acc,R).
 eliminaRepAux([X|XS],Acc,R) :- eliminaRepAux(XS,[X|Acc],R).
 
+% Somatório
+soma([],0).
+soma([X|XS],Total) :- soma(XS, Acumulado), Total is X + Acumulado.
+
 % Concatenar 2 listas
 concat([], R, R).
 concat([X|XS1], R, [X|XS2]) :- concat(XS1, R, XS2).
-
