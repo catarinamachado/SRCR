a11a15d11105d8eb0abed58c1a785c96c4b9c96c
Catarina Machado <catarinamachado11@gmail.com>
2019-03-13 15:42:57 +0000

    Adicionar invariantes

--- exercicio1/exercicio1.pl
+++ exercicio1/exercicio1.pl
@@ -27,21 +27,54 @@
 :- include('calculoDeCustos.pl').
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Invariantes Estruturais e Referenciais
+
+%--------- Utentes
+% Garantir que o id de cada utente é único
++utente(Id,N,I,C) :: (solucoes(Id, utente(Id,_,_,_), R),
+                      comprimento(R, 1)).
+
+% Garantir que utentes com ids diferentes têm diferente informação
++utente(Id,N,I,C) :: (solucoes((N,I,C), utente(_,N,I,C), R),
+                      comprimento(R, 1)).
+
+% Garantir que a idade do utente é válida (>= 0)
++utente(_,_,I,_) :: idadeValida(I).
+
+
+%--------- Serviços
+% Garantir que o id de cada serviço é único
++servico(Id,D,I,C) :: (solucoes(Id, servico(Id,D,I,C), R),
+                      comprimento(R, 1)).
+
+% Garantir que serviços com ids diferentes têm diferente informação
++servico(Id,D,I,C) :: (solucoes((D,I,C), servico(_,D,I,C), R),
+                      comprimento(R, 1)).
+
+
+%--------- Consultas
+% Garantir que o id do utente associado à consulta existe
++consulta(_,IdU,_,_) :: (solucoes(IdU, utente(IdU,_,_,_), R),
+                           comprimento(R, 1)).
+
+% Garantir que o id do serviço associado à consulta existe
++consulta(_,_,IdS,_) :: (solucoes(IdS, servico(IdS,_,_,_), R),
+                           comprimento(R, 1)).
+
+% Garantir que o custo de cada consulta é válido (>= 0)
++consulta(_,_,_,C) :: custoValido(C).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Registar utentes, serviços e consultas
 
 idadeValida(I) :- I >= 0.
 custoValido(C) :- C >= 0.
 
-novoUtente(Id,N,I,C) :- nao(utente(Id,_,_,_)), idadeValida(I),
-                        assert(utente(Id,N,I,C)).
-novoUtente(Id,N,I,C) :- fail.
+novoUtente(Id,N,I,C) :- evolucao(utente(Id,N,I,C)).
 
-novoServico(Id,D,I,C) :- nao(servico(Id,_,_,_)), assert(servico(Id,D,I,C)).
-novoServico(Id,D,I,C) :- fail.
+novoServico(Id,D,I,C) :- evolucao(servico(Id,D,I,C)).
 
-novaConsulta(D,IdU,IdS,C) :- utente(IdU,_,_,_), servico(IdS,_,_,_),
-                             custoValido(C), assert(consulta(D,IdU,IdS,C)).
-novaConsulta(D,IdU,IdS,C) :- fail.
+novaConsulta(D,IdU,IdS,C) :- evolucao(consulta(D,IdU,IdS,C)).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Remover utentes, serviços e consultas
--- exercicio1/funcoesAuxiliares.pl
+++ exercicio1/funcoesAuxiliares.pl
@@ -4,36 +4,33 @@
 % Insucesso
 insucesso :- !, fail.
 
-
 % Calcula o valor de verdade contrário à resposta à questão
 nao(Q) :- Q, insucesso.
 nao(Q).
 
-
 % Encontra todas as soluções
 solucoes(X, XS, L) :- findall(X, XS, L).
 
+% Comprimento de uma lista
+comprimento([], 0).
+comprimento([_|XS], R) :- comprimento(XS, S), R is 1 + S.
 
 % Inserção de conhecimento
 insercao(Q) :- assert(Q).
 insercao(Q) :- retract(Q), insucesso.
 
-
 % Testa se todos os meta-predicados são verdadeiros
 teste([]).
 teste([I|L]) :- I, teste(L).
-
 
 % Insere novo conhecimento na base de conhecimento
 evolucao(T) :- solucoes(I, +T::I, Linv),
                insercao(T),
                teste(Linv).
 
-
 % Averigua se elemento pertence a uma lista
 pertence(A,[A|XS]).
 pertence(A,[X|XS]) :- pertence(A,XS).
-
 
 % Elimina os elementos repetidos numa lista
 eliminaRepetidos(X, R) :- eliminaRepAux(X,[],R).
@@ -41,7 +38,6 @@ eliminaRepetidos(X, R) :- eliminaRepAux(X,[],R).
 eliminaRepAux([],Acc,Acc).
 eliminaRepAux([X|XS],Acc,R) :- pertence(X,Acc), eliminaRepAux(XS,Acc,R).
 eliminaRepAux([X|XS],Acc,R) :- eliminaRepAux(XS,[X|Acc],R).
-
 
 % Somatório
 soma([],0).
