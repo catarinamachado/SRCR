d783d6520c1f69b79003d8a99752c3761607991d
Catarina Machado <catarinamachado11@gmail.com>
2019-04-17 04:08:50 +0100

    Add predicados auxiliares

--- exercicio2/predicadosAuxiliares.pl
+++ exercicio2/predicadosAuxiliares.pl
@@ -3,37 +3,50 @@
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do meta-predicado não: Q -> {V,F}
-% Devolve o valor de verdade contrário a Q
+% Devolve o valor de verdade contrário a Q (negação fraca)
 
-nao( Q ) :- Q, !, fail.
-nao( Q ).
-
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extensao do meta-predicado si: Q,Resposta -> {V,F}
-%                            Resposta = { verdadeiro,falso,desconhecido }
-% capaz de responder a uma única questão
-
-si( Q,verdadeiro ) :-
-    Q.
-si( Q,falso ) :-
-    -Q.
-si( Q,desconhecido ) :-
-    nao( Q ),
-    nao( -Q ).
+nao(Q) :- Q, !, fail.
+nao(Q).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Predicado soluções: X,XS,R -> {V,F}
+% Extensao do predicado soluções: X,XS,R -> {V,F}
 % Encontra todas as soluções
 
-solucoes( X,XS,R ) :- XS, assert(tmp(X)), fail.
-solucoes( X,XS,R ) :- solucoesAux([], R).
+solucoes(X, XS, R) :- XS, assert(tmp(X)), fail.
+solucoes(X, XS, R) :- solucoesAux([], R).
 
-solucoesAux( L,R ) :- retract(tmp(X)), !, solucoesAux( [X|L],R ).
-solucoesAux( R,R ).
+solucoesAux(L, R) :- retract(tmp(X)), !, solucoesAux([X|L], R).
+solucoesAux(R, R).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Predicado comprimento: X,R -> {V,F}
+% Extensao do predicado comprimento: X,R -> {V,F}
 % Devolve o comprimento de uma lista
 
-comprimento( [],0 ).
-comprimento( [_|XS],R ) :- comprimento( XS,S ), R is 1 + S.
+comprimento([],0).
+comprimento([_|XS],R) :- comprimento( XS,S ), R is 1 + S.
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Inserção de conhecimento
+insercao(Q) :- assert(Q).
+insercao(Q) :- retract(Q), !, fail.
+
+% Remoção de conhecimento
+remocao(Q) :- retract(Q).
+remocao(Q) :- assert(Q), !, fail.
+
+% Testa se todos os predicados são verdadeiros
+teste([]).
+teste([I|L]) :- I, teste(L).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Validações
+
+% Idade válida (>= 0)
+idadeValida(I) :- I >= 0.
+
+% Custo válido (>= 0)
+custoValido(C) :- C >= 0.
+
+% Género é masculino ('M') ou feminino ('F')
+generoValido('M').
+generoValido('F').
